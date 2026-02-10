59589a4514c3e87223756c457f9c4b9cd5c32b66
Pankaj Badgujar <pankajb21203@gmail.com>
2026-02-10 04:06:10 -0800

    port change

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from main import app
 if __name__ == "__main__":
     try:
         port = int(os.getenv("PORT", "8005"))
-        uvicorn.run(app, host="10.129.6.47", port=port, reload=False)
+        uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
     except Exception as e:
         print(f"Error starting server: {e}")
         import traceback
